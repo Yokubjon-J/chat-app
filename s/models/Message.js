b1b4b82255fe60bbdfd629f4c061419cae586fc4
Yakubdjon Juraev <yakubdjon.juraev@micros.ucd.uz>
2022-05-31 14:12:47 +0500

    folders and empty files created

--- s/models/Message.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const blogSchema = new Schema({
-    title:  String, // String is shorthand for {type: String}
-    author: String,
-    body:   String,
-    date: { type: Date, default: Date.now },
-});
